a7dd1bdfd6d3dcf88d2edd6a722438b60ca892b1
choieund <cheh020511@gmail.com>
2025-01-02 13:08:46 +0900

    update!

---
+++ netflix/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Nav from './components/Nav'
+import Banner from './components/Banner'
+import Row from './components/Row'
+import Footer from './components/Footer'
+
+function App() {
+  
+   return <div className="App">
+    <Nav />
+    <Banner />
+    <Row />
+    <Footer />
+   </div>
+}
+
+export default App;
