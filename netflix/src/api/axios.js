f8e59e7ada904f551bc922ccb0c1b8750923a0b9
0000yuyu <yujin09219@naver.com>
2025-01-03 02:25:34 +0900

    [2025.01.03] 배너 설정

--- netflix/src/api/axios.js
+++ netflix/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL : "https://api.themoivedb.org/3",
+    baseURL : "https://api.themoviedb.org/3",
     params: {
         api_key: "17629fddd210feb41936f55e0eda83e4",
         language: "ko-KR",
