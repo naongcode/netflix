33f0961d38c5b67566e99a4c618bad8972ca3de7
naong <naong4ha@gmail.com>
2025-01-05 12:08:12 +0900

    app.js에 row.js 연결(props). row.js에서 axios 사용해서 영화정보 가져오고 map이용해서 랜더링 하기 (이미지 보여주기)

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -3,13 +3,26 @@ import Nav from './components/Nav'
 import Banner from './components/Banner'
 import Row from './components/Row'
 import Footer from './components/Footer'
+import requests from './api/request'
 
 function App() {
   
    return <div className="App">
     <Nav />
     <Banner />
-    <Row />
+
+    <Row 
+      title = "NETFLIX ORIGINALS"
+      id = "NO"
+      fetchUrl={requests.fetchNetfilxOriginals} //requests객체에서 가져옴
+      isLargeRow  // 큰 포스터
+    />
+
+    <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending}/>
+    <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated}/>
+    <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies}/>
+    <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies}/>
+
     <Footer />
    </div>
 }
--- netflix/src/components/Row.js
+++ netflix/src/components/Row.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import axios from '../api/axios';
+import React, {useEffect, useState} from 'react'
 
-export default function Row() {
+// <Row />에 들어있는 props(속성)를 가져옴
+// 영화정보 가져올 함수 실행 fetchMovieData()
+export default function Row({ islargeRow, title, id, fetchUrl}) {
+  const [movies, setMovies] = useState([]);
+
+  // fetchUrl이 변할때만 실행
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchUrl])
+  
+  const fetchMovieData = async () => {
+    // axios.get(url주소) -> baseURL과 결합된다.
+    const request = await axios.get(fetchUrl);
+    console.log('request',request);
+
+    // request로 보낸 data.results 정보를 가져옴
+    setMovies(request.data.results)
+  }
+  
   return (
-    <div>Row</div>
+    <section>
+      <h2>{title}</h2>
+
+      {/* 슬라이더 */}
+      <div className='slider'>
+        <div className='slider__arrow-left'>
+          <span className='arrow'>{"<"}</span>
+        </div>
+
+        <div id={id} className='row__posters'>
+          {/* 영화나열하기 정보는 movies에 들어있음*/}
+          {movies.map((movie) => (
+            <img 
+              key={movie.id}
+              className={`row__poster ${islargeRow && "row__posterLarge"}`}
+              // isLargeRow가 적용되어 있으면 poster_path(큰거)아니면 backdrop_path(작은거)
+              src={`https://image.tmdb.org/t/p/original/${islargeRow ? movie.poster_path:movie.backdrop_path}`}
+              loading="lazy"
+              alt={movie.name}
+              />
+          ))}
+        </div>
+
+        <div className='slider__arrow-right'>
+          <span className='arrow'>{">"}</span>
+        </div>
+
+
+      </div>
+
+    </section>
   )
 }
