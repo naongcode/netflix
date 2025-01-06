85fecb0a2a61dbc63fe82b5bed6d5577bc134adb
naong <naong4ha@gmail.com>
2025-01-07 07:34:06 +0900

    모달 open 기능 구현

--- netflix/src/components/Row.js
+++ netflix/src/components/Row.js
@@ -1,11 +1,14 @@
 import axios from '../api/axios';
 import React, {useEffect, useState} from 'react'
 import "./Row.css"
+import MovieModal from "./MovieModal"
 
 // <Row />에 들어있는 props(속성)를 가져옴
 // 영화정보 가져올 함수 실행 fetchMovieData()
 export default function Row({ islargeRow, title, id, fetchUrl}) {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({})
 
   // fetchUrl이 변할때만 실행
   useEffect(() => {
@@ -15,12 +18,18 @@ export default function Row({ islargeRow, title, id, fetchUrl}) {
   const fetchMovieData = async () => {
     // axios.get(url주소) -> baseURL과 결합된다.
     const request = await axios.get(fetchUrl);
-    console.log('request',request);
+    // console.log('request',request);
 
     // request로 보낸 data.results 정보를 가져옴
     setMovies(request.data.results)
   }
   
+  const handleClick = (movie) => {
+    setModalOpen(true)
+    setMovieSelected(movie);
+    console.log('movie',movie)
+  }
+
   return (
     <section className='row'>
       <h2>{title}</h2>
@@ -36,6 +45,7 @@ export default function Row({ islargeRow, title, id, fetchUrl}) {
           {movies.map((movie) => (
             <img 
               key={movie.id}
+              onClick={()=> handleClick(movie)} // 클릭시 모달함수실행
               className={`row__poster ${islargeRow && "row__posterLarge"}`}
               // isLargeRow가 적용되어 있으면 poster_path(큰거)아니면 backdrop_path(작은거)
               src={`https://image.tmdb.org/t/p/original/${islargeRow ? movie.poster_path : movie.backdrop_path}`}
@@ -51,6 +61,13 @@ export default function Row({ islargeRow, title, id, fetchUrl}) {
 
 
       </div>
+
+      {/* 모달열기 */}
+      {modalOpen && 
+        (<MovieModal 
+          movieId={movieSelected.id} 
+          {...movieSelected} 
+          setModalOpen={setModalOpen}/>)}
 
     </section>
   )
