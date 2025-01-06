400d2761df38429646ae75ccd009a105f4f18f73
song-yujin <hakty6203@naver.com>
2025-01-06 14:28:46 +0900

    [2025.01.06] 내비게이션 페이지 전환 및 검색 페이지 구현

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,30 +1,37 @@
 import './App.css';
 import Nav from './components/Nav'
-import Banner from './components/Banner'
-import Row from './components/Row'
 import Footer from './components/Footer'
-import requests from './api/request'
+import {BrowserRouter,Routes,Route,Outlet} from 'react-router-dom'
+import MainPage from './components/MainPage';
+import DetailPage from './components/DetailPage'
+import SearchPage from './components/SearchPage'
 
-function App() {
-  
-   return <div className="App">
-    <Nav />
-    <Banner />
+function Layout () {
+   return (
+      <div>
+         <Nav />
 
-    <Row 
-      title = "NETFLIX ORIGINALS"
-      id = "NO"
-      fetchUrl={requests.fetchNetfilxOriginals} //requests객체에서 가져옴
-      isLargeRow  // 큰 포스터
-    />
+         <Outlet />
 
-    <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending}/>
-    <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated}/>
-    <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies}/>
-    <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies}/>
-
-    <Footer />
-   </div>
+         <Footer />
+      </div>
+   )
 }
 
+function App() {
+   return(
+      <div className='App'>
+      <BrowserRouter>
+      <Routes> 
+         {/* 레이아웃 적용 */}
+      <Route path="/" element={<Layout />} >
+         <Route index element={<MainPage />} />
+         <Route path=":movieId" element={<DetailPage />} />
+         <Route path="search" element={<SearchPage />} />
+      </Route>
+      </Routes>
+   </BrowserRouter>
+   </div>
+   )
+}
 export default App;
